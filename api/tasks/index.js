8c4145401f1982b077a53c51b432320f8e995545
jakobtivell-cpu <jakob.tivell@gmail.com>
2026-02-04 20:57:16 +0100

    Add files via upload

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -1,1 +1,8 @@
-
+const getDb = require('../_shared/db');
+module.exports = async function (context) {
+  const db = await getDb();
+  const r = await db.request().query(
+    'SELECT TaskId, TaskName FROM Task WHERE IsActive=1'
+  );
+  context.res.json(r.recordset);
+};
