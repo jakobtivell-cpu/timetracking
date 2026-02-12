c8ad1075cfbdef5983be2242a74ecaafe61a7d6d
jakobtivell-cpu <jakob.tivell@gmail.com>
2026-02-12 13:47:45 +0100

    Delete api directory

--- api/timeentries/start.js
+++
@@ -1,9 +0,0 @@
-const getDb = require('../_shared/db');
-module.exports = async function (context, req) {
-  const { taskId } = req.body;
-  const db = await getDb();
-  const r = await db.request()
-    .input('TaskId', taskId)
-    .query('EXEC dbo.sp_TimeEntry_Start @TaskId=@TaskId');
-  context.res.json(r.recordset[0] || { ok:true });
-};
--- api/timeentries/stop.js
+++
@@ -1,9 +0,0 @@
-const getDb = require('../_shared/db');
-module.exports = async function (context, req) {
-  const { timeEntryId } = req.body;
-  const db = await getDb();
-  await db.request()
-    .input('TimeEntryId', timeEntryId)
-    .query('EXEC dbo.sp_TimeEntry_StopNow @TimeEntryId=@TimeEntryId');
-  context.res.json({ ok:true });
-};
